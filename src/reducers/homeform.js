3ae1c30a4e15249b2ba21e7dd07f5b9d7d1e88dd
Louis-Le-Croller <louis.le-croller@oclock.school>
2023-12-05 15:29:16 +0100

    CLEAN - Removed InformationForm and RegistrationForm

--- src/reducers/homeform.js
+++ src/reducers/homeform.js
@@ -1,25 +1,8 @@
-import { CHANGE_INFO_FIELD } from '../actions/homeform';
-
 // reducer for homepage forms (info request and subscribtion)
-export const initialState = {
-  firstname: '',
-  lastname: '',
-  email: '',
-  organizationName: '',
-  organizationType: '',
-  phoneNumber: '',
-  address: '',
-  siren: '',
-  message: '',
-};
+export const initialState = {};
 
 const reducer = (state = initialState, action = {}) => {
   switch (action.type) {
-    case CHANGE_INFO_FIELD:
-      return {
-        ...state,
-        [action.identifier]: action.newValue,
-      };
     default:
       return state;
   }
